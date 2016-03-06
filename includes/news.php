6f91ed46dca9781770f20b2a62089859603c2f79
NikhoGabriel <nicolas.dumouchel@gmail.com>
2016-03-06 13:59:01 +0100

    Update news.php

--- includes/news.php
+++ includes/news.php
@@ -67,5 +67,116 @@
 		</section><?php
 		}
 	}
+	
+	// Forums récents //
+	
+	if ($_SESSION['rank'] >= rank_disp_all_forum)
+			{
+				$answer = $db->prepare('SELECT * FROM forum_category ORDER BY rank, name');
+				$params = array ();
+			}
+			else
+			{
+				$answer = $db->prepare('SELECT * FROM forum_category WHERE rank <= ? ORDER BY rank, name');
+				$params = array ($_SESSION['rank']);
+			}
+			$dispForum = true;
+			$categoryPage = false;
+		}
+		
+		if ($dispForum)
+		{
+		$answer->execute($params);
+		
+		?><h3>Activité récente</h3><?php
+		if (!$categoryPage)
+		{
+			?>
+				<ul id="categories">
+			<?php
+		}
+		while ($line = $answer->fetch())
+		{
+			//Affichage de l'en-tête de la catégorie
+			if ($categoryPage)
+			{
+				?><h4><?= $line['name']?></h4><?php
+				$answer2 = $db->prepare('SELECT f.id AS id, f.name AS f_name, f.important important, p.post_date AS date, m.id AS m_id, m.name AS m_name, m.rank AS rank,m.title AS title,m.pionier AS pionier, u.unread AS unread
+						 FROM forum_post AS p
+						 RIGHT JOIN forum_forum AS f ON f.last_post = p.id
+						 LEFT JOIN members AS m ON m.id = p.user_id
+						 LEFT JOIN forum_unread AS u ON ? = u.user_id AND f.id = u.forum_id
+						 WHERE f.category = ? 
+						 ORDER BY f.important DESC, p.post_date DESC');
+					
+					 if ($line['pionier'] == 1) { $rank = "Pionier";} else {  $rank = $line['rank'] ;}
+			}
+			else
+			{
+				?>
+				<li class="forum_category"><p><a href="index.php?p=forum&amp;category=<?= $line['id']?>"><?echo $rank?> <?= $line['name']?></a></p>
+				
+				<img style="margin-left: 20px;" src="pics/forumcat_<?= $line['id']?>.png" alt=" " class="guild" />
+				
+				<?php
+				$answer2 = $db->prepare('SELECT f.id AS id, f.name AS f_name, f.important important,f.rp rp, p.post_date AS date, m.id AS m_id, m.name AS m_name, m.rank AS rank, m.title AS title, m.pionier pionier,m.technician technician, u.unread AS unread
+						 FROM forum_post AS p
+						 RIGHT JOIN forum_forum AS f ON f.last_post = p.id
+						 LEFT JOIN members AS m ON m.id = p.user_id
+						 LEFT JOIN forum_unread AS u ON ? = u.user_id AND f.id = u.forum_id
+						 WHERE f.category = ? AND unread = 0
+						 ORDER BY f.important DESC, p.post_date DESC
+						 LIMIT 10');
+			}
+			?><table width="100%" class="forum" cellspacing="0" cellpadding="0">
+				<tr class="head_table">
+					<th>Sujet</th>
+					<th class="last_post">Dernière participation</th>
+				</tr>
+				
+				<?php
+			//Affichage des forums de la catégorie
+			$answer2->execute(array($_SESSION['id'], $line['id']));
+			$void = true;
+			while ($line2 = $answer2->fetch())
+			{	$filename = 'pics/avatar/miniskin_' .$line2['m_id']. '.png';if (file_exists($filename)) {$skin = $line2['m_id'];} else {$skin = 'no';}
+				$void = false;
+				if ($line2['pionier'] == 1) { $pionier = "-P";} else { $pionier = '';} if ($line2['technician'] == 1) { $tech = "-T";} else { $tech = '';}
+				
+				?><tr>
+					<td
+					<?php echo (isset($line2['unread']) && $line2['unread']) ? 'class="unread" ' : 'class="read" '; ?>
+					><img src="pics/forumrp_<?= $line2['rp'] ?>.png" width="30px" alt=" " /> <a href="index.php?p=forum&amp;forum=<?= $line2['id']?>"><?php if ($line2['rp'] == 1) { echo "[RP] ";}
+					echo ($line2['important']) ? '[Important] ':'';?><?= $line2['f_name']?></a><?php
+				if ($_SESSION['rank'] >= rank_del_forum)
+				{
+					?><form method="POST" action="index.php?p=forum" class="del_form">
+						<input type="button" value="[X]" class="del_button"/>
+						<input type="hidden" name="action" value="del_forum" />
+						<input type="hidden" name="forum" value="<?= $line2['id']?>" />
+					</form><?php
+				}
+				$date = preg_replace('#^(.{4})-(.{2})-(.{2}) (.{2}:.{2}):.{2}$#', 'Le $3/$2/$1 à $4', $line2['date']);
+				if ($line2['rank'])
+				{
+				?></td>
+				<td
+				<?php echo (isset($line2['unread']) && $line2['unread']) ? 'class="unread" ' : ' '; ?>
+				><img src="pics/avatar/miniskin_<? echo $skin?>.png" width="20px" /> <a href="index.php?p=viewmember&perso=<?= $line2['m_id']?>" class="name<?= $line2['rank']?><?echo $tech ?><? echo $pionier?>">
+				
+				<? //Affichage du titre "Pionier" //
+				if ($line2['pionier'] == 1) { echo "Pionier";} else { echo $line2['title'] ;}?>
+				
+				<?= $line2['m_name']?></a> <br /> <?= $date?></td>
+				</tr><?php
+				}
+			}
+			if ($void)
+			{
+				?><tr><td>Catégorie vide.</td></tr><?php
+			}
+			
+			?></table></li>
+	<?php
 }
 ?>
